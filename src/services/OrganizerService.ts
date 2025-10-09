b5b5e718872c403411d6c0ec8206c277b286edaa
Evan Clark <black0ashes@gmail.com>
2025-10-10 03:18:51 +0700

    l9f VIV (6.4)

--- src/services/OrganizerService.ts
+++ src/services/OrganizerService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // must be http://localhost:8080
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -10,13 +10,16 @@ const apiClient = axios.create({
 })
 
 export default {
-  getOrganizers(perPage: number, page: number) {
-    return apiClient.get('/organizers?_limit=' + perPage + '&_page=' + page)
-  },
-  getOrganizer(id: number) {
-    return apiClient.get('/organizers/' + id)
-  },
-  createOrganizer(organizer: any) {
-    return apiClient.post('/organizers', organizer)
+  getOrganizers() {
+    return apiClient.get('/organizers')
   }
+  // getOrganizers(perPage: number, page: number) {
+  //   return apiClient.get('/organizers?_limit=' + perPage + '&_page=' + page)
+  // },
+  // getOrganizer(id: number) {
+  //   return apiClient.get('/organizers/' + id)
+  // },
+  // createOrganizer(organizer: any) {
+  //   return apiClient.post('/organizers', organizer)
+  // }
 }
--- src/views/event/EventFormView.vue
+++ src/views/event/EventFormView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import type { Event } from '@/types'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import EventService from '@/services/EventService'
 import { useRouter } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import BaseInput from '@/components/BaseInput.vue'
+import type { Event, Organizer } from '@/types'
+import OrganizerService from '@/services/OrganizerService'
 
 const event = ref<any>({
     category: '',
@@ -35,6 +36,17 @@ function saveEvent() {
             router.push({ name: 'network-error-view' })
         })
 }
+
+const organizers = ref<Organizer[]>([])
+onMounted(() => {
+    OrganizerService.getOrganizers()
+        .then((response) => {
+            organizers.value = response.data
+        })
+        .catch(() => {
+            router.push({ name: 'network-error-view' })
+        })  
+})
 </script>
 
 <template>
@@ -47,6 +59,17 @@ function saveEvent() {
             <BaseInput v-model="event.description" type="text" label="Description" />
             <h3>Where is your event?</h3>
             <BaseInput v-model="event.location" type="text" label="Location" />
+            <h3>who is your organizer?</h3>
+            <label>Select an Organizer</label>
+            <select v-model="event.organizer.id">
+              <option
+                v-for="option in organizers"
+                :value="option.id"
+                :key="option.id"
+                :selected="option.id === event.organizer.id"
+                > {{ option.name }}
+              </option>
+            </select>
             <button class="button" type="submit">Submit</button>
         </form>
 
